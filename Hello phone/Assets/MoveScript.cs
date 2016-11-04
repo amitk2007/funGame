b75c9b992c1438723908b78d6b82bd6f891d7a81
amitk2007 <amitk2007@gmail.com>
2016-11-04 14:22:23 +0200

    firstcomite

---
+++ Hello phone/Assets/MoveScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+	bool touching = false;
+	public Material[] met = new Material[2];
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.touchCount==1/*&&touching == false*/) {
+			gameObject.GetComponent<Renderer>().material = met[0];
+			//transform.Translate(Input.touches[0].deltaPosition.x*0.25f,Input.touches[0].deltaPosition.y*0.25f,0);
+			//this.gameObject.transform.Translate(new Vector3(0,1,0));
+			this.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.touches[0].position.x,Input.touches[0].position.y,10));
+			/*touching = true;*/
+		}
+		if (Input.touchCount == 0) {
+			gameObject.GetComponent<Renderer>().material = met[1];
+			/*touching = false;*/
+		}
+
+	}
+}
---
+++ Hello phone/Assets/TouchScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Input.touches -=- array
+		//Touch myTouch = Input.GetTouch (0); -=- individual Touch
+	}
+
+	void OnGUI()
+	{
+		foreach (Touch touch in Input.touches)
+		{
+			string message = "";
+			message += "ID: " +touch.fingerId +"\n";
+			message += "Phase: " +touch.phase.ToString() +"\n";
+			message += "TapCount: " + touch.tapCount +"\n";
+			message += "Pos: " + touch.position +"\n";
+
+			int num = touch.fingerId;
+			GUI.Label(new Rect (0+num*120,0,120,100),message);
+		}
+	}
+}
